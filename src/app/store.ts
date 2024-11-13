85823419325e5fca881ce95c19e0553691601b04
Jaime de Venecia <deveneciaj@gmail.com>
2024-11-13 15:49:49 -0800

    configure store

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+import type { Action } from '@reduxjs/toolkit'
+
+interface CounterState {
+  value: number
+}
+
+function counterReducer(state: CounterState = { value: 0 }, action: Action) {
+  switch (action.type) {
+    default: {
+      return state
+    }
+  }
+}
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+})
