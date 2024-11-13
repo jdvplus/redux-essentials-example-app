8c3c17d47e1887455095d83479d5ad2dfbe78ebc
Jaime de Venecia <deveneciaj@gmail.com>
2024-11-13 15:55:40 -0800

    connect app to store

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
+import './index.css'
+import './primitiveui.css'
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-
-import App from './App'
+import { Provider } from 'react-redux'
 
 import { worker } from './api/server'
-
-import './primitiveui.css'
-import './index.css'
+import App from './App'
+import { store } from './app/store'
 
 // Wrap app rendering so we can wait for the mock API to initialize
 async function start() {
@@ -17,7 +17,9 @@ async function start() {
 
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   )
 }
